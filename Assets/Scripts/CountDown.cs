2596166dfc37f02a7f38047c415ca98c9e44e7a4
Will M. 3 <willmcb234@gmail.com>
2019-10-03 15:09:39 -0400

    10.3.2019_Update

---
+++ Assets/Scripts/CountDown.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Threading;
+
+public class CountDown : MonoBehaviour
+{
+public int timeLeft = 60;
+public Text countDown;
+public Text GameOver; 
+public GameObject MPannel;
+    // Start is called before the first frame update
+    void Start()
+    {
+    
+StartCoroutine("LoseTime");
+Time.timeScale = 1; 
+MPannel = GameObject.Find("Main_Menu_Pannel");
+MPannel.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    countDown.text = ("" + timeLeft);
+    if(timeLeft <= 0)
+    {
+    timeLeft = 0;
+    GameOver.text = ("Game Over");
+    MPannel.SetActive(true);
+    }
+    }
+IEnumerator LoseTime()
+{
+while(true)
+{
+yield return new WaitForSeconds(1);
+timeLeft --; 
+}
+}
+}
