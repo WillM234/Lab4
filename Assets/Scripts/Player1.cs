72b5422145f65c2e0ae4d71985992b4ae5ed2006
Will M. 3 <willmcb234@gmail.com>
2019-10-06 15:18:43 -0400

    10.6.2019_Update

---
+++ Assets/Scripts/Player1.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player1 : MonoBehaviour
+{
+private Rigidbody2D rb2d;
+private float speed = .15f;
+private float boundX = 8f;
+    // Start is called before the first frame update
+    void Start()
+    {
+rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      if (Input.GetKey(KeyCode.A))//move right
+        {
+            transform.Translate(Vector2.right * speed);
+        }
+    if (Input.GetKey(KeyCode.D))//move left
+        {
+            transform.Translate(Vector2.left * speed);
+        }
+     var pos = transform.position;
+        if(pos.x > boundX)
+        {
+            pos.x = boundX;
+        }
+        else if(pos.x < -boundX)
+        {
+            pos.x = -boundX;
+        }
+
+        transform.position = pos;
+    }
+}
---
+++ Assets/Scripts/ScoreSystem.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ScoreSystem : MonoBehaviour
+{
+public int Score;
+public Text countUp;
+public GameObject Apple;
+    // Start is called before the first frame update
+    void Start()
+    {
+   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    countUp.text = ("Score: " + Score);
+    }
+    void OnCollisionEnter2D(Collision2D other)
+    {
+    if(other.gameObject.tag == "Ground")
+        {
+        Score = 0;
+    Destroy(gameObject);
+        }
+    if(other.gameObject.tag == "Player")
+        {
+        Score += 1; 
+    Destroy(gameObject,0.5f);
+        }
+    }
+}
